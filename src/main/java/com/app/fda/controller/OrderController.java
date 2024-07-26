2e25a899465164ab79707815fd63a997440d950c
mriduldeka <mridul.deka09@gmail.com>
2024-07-26 18:11:45 +0530

    Initial commit

---
+++ src/main/java/com/app/fda/controller/OrderController.java
@@ -0,0 +1,13 @@
+package com.app.fda.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+    @GetMapping("/hello")
+    public String index() {
+        System.out.println("print orders from FDA");
+        return "orders from FDA";
+    }
+}
